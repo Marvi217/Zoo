af9b4578b93dffbd95dabddf98f7ac5c824ea2b0
Tomasz Niedziółka <162788508+Marvi217@users.noreply.github.com>
2026-01-28 21:29:49 +0100

    fix

--- src/main/java/com/example/zoo/ZooApplication.java
+++ src/main/java/com/example/zoo/ZooApplication.java
@@ -3,8 +3,12 @@ package com.example.zoo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class ZooApplication {
 
     public static void main(String[] args) {
